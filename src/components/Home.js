fe1d6a9caf0dc8feb106ec2e86ea879526fa0e08
kajamal <75475723+kajamal@users.noreply.github.com>
2025-08-19 18:01:31 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
        <div className="container">
          <h2>Welcome kaja sharif</h2>
          <p>
-           Discover the world of delicious crackers??
+           Discover the world of delicious kaja sharif crackers??
          </p>
 
          {/* Links to other sections */}
